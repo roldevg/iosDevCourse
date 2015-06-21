bf1f9dab2fd559456e2a7df63d02e30e42026af4
Evgeniy Roldukhin <evgeniy@roldukhin.ru>
2015-06-21 11:16:31 +0300

    change color in cell with rotate screen

--- ChessDesk/ChessDesk/ViewController.m
+++ ChessDesk/ChessDesk/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) UIView *containerForBoard;
 @end
 
 @implementation ViewController
@@ -26,30 +26,41 @@
     CGFloat screenWidth = self.view.bounds.size.width;
     CGFloat screenHeight = self.view.bounds.size.height;
     
-    UIView *containerForBoard = [[UIView alloc] initWithFrame:CGRectMake(0, (screenHeight/2 - screenWidth/2), screenWidth, screenWidth)];
-    containerForBoard.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin |
-                                         UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
-    containerForBoard.backgroundColor = [UIColor blackColor];
-    [self.view addSubview:containerForBoard];
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, (screenHeight/2 - screenWidth/2), screenWidth, screenWidth)];
+    self.containerForBoard = view;
+    self.containerForBoard.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin |
+                                              UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
+    self.containerForBoard.backgroundColor = [UIColor whiteColor];
+    self.containerForBoard.layer.borderColor = [UIColor blackColor].CGColor;
+    self.containerForBoard.layer.borderWidth = 2.0f;
+    [self.view addSubview:self.containerForBoard];
     
     int numberOfCells = 8;
-    CGFloat chessBoardWidth = containerForBoard.bounds.size.width - 4 * 2; // left and right bounds
+    CGFloat chessBoardWidth = self.containerForBoard.bounds.size.width - 4 * 2; // left and right bounds
     CGFloat chessBoardCellWidth = chessBoardWidth / numberOfCells ;
     
     for (int i = 0; i < 8; i++) {
-        
         for (int j = 0; j < 8; j++) {
-            
-            UIView *cellView = [[UIView alloc] initWithFrame:CGRectMake(chessBoardCellWidth * i + 4, chessBoardCellWidth * j + 4,
-                                                                        chessBoardCellWidth, chessBoardCellWidth)];
-            [containerForBoard addSubview:cellView];
-            
             if ((i + j) % 2 == 0) {
+                UIView *cellView = [[UIView alloc] initWithFrame:CGRectMake(chessBoardCellWidth * j + 4, chessBoardCellWidth * i + 4,
+                                                                            chessBoardCellWidth, chessBoardCellWidth)];
                 cellView.backgroundColor = [UIColor blackColor];
-            } else {
-                cellView.backgroundColor = [UIColor whiteColor];
+                [self.containerForBoard addSubview:cellView];
             }
         }
+    }
+}
+
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation {
+    
+    CGFloat r = (CGFloat) (arc4random() % 255) / 255.f;
+    CGFloat g = (CGFloat) (arc4random() % 255) / 255.f;
+    CGFloat b = (CGFloat) (arc4random() % 255) / 255.f;
+    
+    UIColor *newColor = [UIColor colorWithRed:r green:g blue:b alpha:0.8];
+    
+    for (UIView *view in self.containerForBoard.subviews) {
+        view.backgroundColor = newColor;
     }
 }
 
