f552c4c88f49e67ebaf8260d96fb1cd218b8ad5e
Evgeniy Roldukhin <evgeniy@roldukhin.ru>
2015-06-28 12:24:46 +0300

    init project "work with structure"

---
+++ Structures/Structures/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Structures
+//
+//  Created by Evgeniy Roldukhin on 6/28/15.
+//  Copyright (c) 2015 Evgeniy Roldukhin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
