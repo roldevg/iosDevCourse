6c76d9be2e82eb8c2a72f3063f1bacf1ff4b63a9
Evgeniy Roldukhin <evgeniy@roldukhin.ru>
2015-07-01 23:56:58 +0300

    work with basic animation

---
+++ Animations/Animations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Animations
+//
+//  Created by Evgeniy Roldukhin on 7/1/15.
+//  Copyright (c) 2015 Evgeniy Roldukhin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Animations/Animations/ViewController.m
@@ -0,0 +1,60 @@
+//
+//  ViewController.m
+//  Animations
+//
+//  Created by Evgeniy Roldukhin on 7/1/15.
+//  Copyright (c) 2015 Evgeniy Roldukhin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) UIView* testView;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIView* view = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+    view.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:view];
+    
+    self.testView = view;
+}
+
+- (void) viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+/*    [UIView animateWithDuration:5
+                     animations:^{
+                         
+                         self.testView.center = CGPointMake(CGRectGetWidth(self.view.bounds) - CGRectGetWidth(self.testView.frame) / 2, 150);
+                         
+                     }];*/
+    
+    [UIView animateWithDuration:2
+                          delay:1
+                        options:UIViewAnimationOptionCurveLinear | UIViewAnimationOptionRepeat | UIViewAnimationOptionAutoreverse
+                     animations:^{
+                         self.testView.center = CGPointMake(CGRectGetWidth(self.view.bounds) - CGRectGetWidth(self.testView.frame) / 2, 150);
+                     }
+                     completion:^(BOOL finished) {
+                         NSLog(@"Animation finished! %d", finished);
+                     }];
+    
+    double delayInSeconds = 3;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self.testView.layer removeAllAnimations];
+    });
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
