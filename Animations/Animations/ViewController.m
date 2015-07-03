9177b6bec9be112b6f5eac40e4985d9b00f6d2fa
Evgeniy Roldukhin <evgeniy@roldukhin.ru>
2015-07-03 22:45:13 +0300

    create random animations

--- Animations/Animations/ViewController.m
+++ Animations/Animations/ViewController.m
@@ -28,28 +28,49 @@
 - (void) viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
     
-    [UIView animateWithDuration:5
-                     animations:^{
-                         
-                         self.testView.center = CGPointMake(CGRectGetWidth(self.view.bounds) - CGRectGetWidth(self.testView.frame) / 2, 150);
-                         
-                     }];
+    [self moveView:self.testView];
+}
+
+- (void) moveView:(UIView*) view {
     
-    [UIView animateWithDuration:2
-                          delay:1
-                        options:UIViewAnimationOptionCurveLinear | UIViewAnimationOptionRepeat | UIViewAnimationOptionAutoreverse
+    CGRect rect = self.view.bounds;
+    rect = CGRectInset(rect, CGRectGetWidth(view.frame), CGRectGetHeight(view.frame));
+    
+    CGFloat x = arc4random() % (int)CGRectGetWidth(rect) + CGRectGetMinX(rect);
+    CGFloat y = arc4random() % (int)CGRectGetHeight(rect) + CGRectGetMinY(rect);
+    
+    CGFloat newScale = (float)(arc4random() % 151) / 100.f + 0.5f;
+    
+    // from 0 to 2pi
+    CGFloat r = (double)(arc4random() % (int)(M_PI * 2 * 10000)) / 10000 - M_PI;
+    
+    CGFloat d = (float)(arc4random() % 20001) / 10000 + 2;
+    
+    [UIView animateWithDuration:d
+                          delay:0
+                        options:UIViewAnimationOptionCurveLinear // | UIViewAnimationOptionRepeat | UIViewAnimationOptionAutoreverse
                      animations:^{
-                         self.testView.center = CGPointMake(CGRectGetWidth(self.view.bounds) - CGRectGetWidth(self.testView.frame) / 2, 150);
+                         // view.center = CGPointMake(CGRectGetWidth(self.view.bounds) - CGRectGetWidth(view.frame) / 2, 150);
+                         view.center = CGPointMake(x, y);
+                         
+                         CGAffineTransform rotation = CGAffineTransformMakeRotation(r);
+                         CGAffineTransform scale  = CGAffineTransformMakeScale(newScale, newScale);
+                         CGAffineTransform transform = CGAffineTransformConcat(scale, rotation);
+                         view.transform = transform;
                      }
                      completion:^(BOOL finished) {
                          NSLog(@"Animation finished! %d", finished);
+                         NSLog(@"\nView frame = %@\nView bounds = %@", NSStringFromCGRect(view.frame), NSStringFromCGRect(view.bounds));
+                         
+                         __weak UIView* v = view;
+                         
+                         [self moveView:v];
                      }];
     
-    double delayInSeconds = 3;
+    double delayInSeconds = 13;
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [self.testView.layer removeAllAnimations];
+        [view.layer removeAllAnimations];
     });
-
 }
 
 - (void)didReceiveMemoryWarning {
